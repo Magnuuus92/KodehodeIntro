a472e47a9452873d092b0d98dae201afdd1409be
Magnuuus92 <Magnus92.kodehode@gmail.com>
2026-02-02 13:27:58 +0100

    freecodecamp2

---
+++ øvinger/fcc2/Program.cs
@@ -0,0 +1,8 @@
+﻿int fahrenheit = 94;
+double fahrenheitDo = fahrenheit;
+double celsius = (fahrenheitDo - 32) * 5 / 9;
+Console.WriteLine(celsius);
+
+decimal celsiusDe = (fahrenheit - 32m) * (5m / 9m);
+Console.WriteLine(celsiusDe);
+Console.WriteLine(5 / 10);
